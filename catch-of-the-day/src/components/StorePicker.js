81ac5747f64ca91fb95a5b9d2e36a6d7f1ef5447
DraciVik <viktor.stojanov90@gmail.com>
2018-09-18 09:33:30 +0200

    Done with 11 of the videos

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,17 +1,35 @@
 import React from "react";
-import { format } from "util";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-                <form className="store-selector">
-                {/*Hi */}
-                    <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name" />
-                    <button type="submit">Visit Store -></button>
-                </form>
-        )
-    }
+  constructor() {
+    super();
+    this.goToStore = this.goToStore.bind(this);
+  }
+  myInput = React.createRef();
+
+  goToStore(event) {
+    // 1. Stop the form from submitting 
+    event.preventDefault();
+    // 2. Get the text from that input
+    
+    // 3. Change the page from /store/whatever=they-entered
+  }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
 }
 
 export default StorePicker;
